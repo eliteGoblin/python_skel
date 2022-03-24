78cd7c0ce23132f6512914462e479cc1a1a336cd
Frank Sun <frank.sun@nearmap.com>
2022-03-24 12:05:19 +1100

    fail CI

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import List
+
+
+
+
 
 import math
-from typing import List
+
 
 from package_outter import outter
 
